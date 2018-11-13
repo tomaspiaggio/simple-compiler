94c2d4f5a7431c0457d8bf6495e778244561ef9d
Tomas Piaggio <tomas.piaggio12@gmail.com>
2018-11-13 16:47:30 -0300

    Changed everything to not differentiate between double and int

---
+++ src/main/java/expression/impl/ConditionBooleanExpression.java
@@ -0,0 +1,21 @@
+package expression.impl;
+
+import condition.Condition;
+import expression.Expression;
+import java.util.HashMap;
+
+public class ConditionBooleanExpression implements Expression {
+
+    Expression e, e2;
+    Condition c;
+
+    public ConditionBooleanExpression(Expression e, Condition c, Expression e2) {
+        this.e = e;
+        this.c = c;
+        this.e2 = e2;
+    }
+
+    public Object run(HashMap<String, Object> hm) {
+        return c.test(e, e2, hm);
+    }
+}
